1dacce1f984e27ec995028abfd8449d73a18a57a
kiptoo <denysmutay@gmail.com>
2021-06-12 13:01:13 +0300

    added migrations

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,24 @@
-from flask import render_template
+from flask import render_template, redirect, flash, url_for
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
+@app.route('/home')
 def home():
 	title = 'Home'
 	return render_template('home.html', title=title)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET','POST'])
 def login():
 	title = 'Login'
-	return render_template('login.html', title=title)
+	form = LoginForm()
+	if form.validate_on_submit():
+		flash('Login requested for user {}, remember_me={}'.format(
+			form.username.data, form.remember_me.data))
+		return redirect(url_for("home"))
+	return render_template('login.html', title=title, form=form)
 
 
 @app.route('/signup')
